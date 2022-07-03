34260c1e26384dc07ee6938f5c1a0968d40978d9
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-07-03 14:49:49 +0200

    add redirect to GitHub on index page

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	http_response_code(302);
+	header("Location: https://github.com/FreekBes/improved_intra");
+?>
+<p>View Improved Intra 42 on <a href="https://github.com/FreekBes/improved_intra">GitHub</a></p>
