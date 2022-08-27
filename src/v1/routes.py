e04731a98294bfcf14e42108741e3208a732d227
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-08-27 19:20:49 +0200

    create helpers file and move old settings there

--- src/v1/routes.py
+++ src/v1/routes.py
@@ -2,11 +2,11 @@ from flask import session, jsonify, request, redirect, url_for, render_template
 import logging
 import json
 import time
-from urllib.parse import urlparse
-from .. import app, db
-from ..models import ColorScheme, BannerImg, BannerPosition, Profile, Settings, User, OAuth2Token
+from .. import app
+from ..models import OAuth2Token
 from ..oauth import authstart
 from .forms import OldSettings
+from .helpers import get_v1_settings
 
 logging.basicConfig(filename=app.config['LOG_FILE'], level=logging.DEBUG, format=app.config['LOG_FORMAT'])
 
@@ -26,44 +26,10 @@ def oldConnect():
 
 @app.route('/settings/<login>.json', methods=['GET'])
 def oldSettings(login):
-	try:
-		db_user = db.session.query(User.intra_id, User.login).filter(User.login == login).one()
-		db_settings = db.session.query(Settings).filter(Settings.user_id == db_user.intra_id).one() # This query can be sped up by selecting only what is needed in the future
-		db_colors = db.session.query(ColorScheme.internal_name).filter(ColorScheme.id == db_settings.colors).one()
-		db_profile = db.session.query(Profile.banner_img, Profile.banner_pos, Profile.link_git).filter(Profile.user_id == db_user.intra_id).one()
-		db_banner_pos = db.session.query(BannerPosition.internal_name).filter(BannerPosition.id == db_profile.banner_pos).one()
-		db_banner_img = None
-		if db_profile.banner_img:
-			db_banner_img = db.session.query(BannerImg.url).filter(BannerImg.id == db_profile.banner_img).first()
-	except:
-		return '404 Not Found', 404
-	resp = {
-		'username': db_user.login,
-		'sync': True,
-
-		'ext_version': db_settings.updated_ver,
-		'theme': "dark" if db_settings.theme == 2 else "light" if db_settings.theme == 3 else "system",
-		'colors': db_colors.internal_name,
-		'show-custom-profiles': db_settings.show_custom_profiles,
-		'hide-broadcasts': db_settings.hide_broadcasts,
-		'logsum-month': db_settings.logsum_month,
-		'logsum-week': db_settings.logsum_week,
-		'outstandings': db_settings.outstandings,
-		'hide-goals': db_settings.hide_goals,
-		'holygraph-morecursuses': db_settings.holygraph_more_cursuses,
-		'old-blackhole': db_settings.old_blackhole,
-		'clustermap': db_settings.clustermap,
-		'codam-monit': db_settings.codam_monit,
-		'codam-buildingtimes-public': False,
-		'codam-buildingtimes-chart': False,
-		'codam-auto-equip-coa-title': True,
-		'timestamp': int(db_settings.updated_at.timestamp()),
-
-		'link-github': urlparse(db_profile.link_git).path.lstrip('/') if db_profile.link_git else '',
-		'custom-banner-url': db_banner_img.url if db_banner_img else '',
-		'custom-banner-pos': db_banner_pos.internal_name
-	}
-	return jsonify(resp), 200
+	old_settings = get_v1_settings(login)
+	if old_settings:
+		return jsonify(old_settings), 200
+	return '404 Not Found', 404
 
 
 @app.route('/options.php', methods=['GET'])
@@ -103,7 +69,8 @@ def oldUpdate():
 		return {'type': 'error', 'message': 'Invalid value for GET key \'v\'', 'v': request.args['v']}, 400
 	form = OldSettings(request.form)
 	if form.validate():
-		return {'type': 'success', 'message': 'Settings saved', 'data': request.form}, 201
+		# TODO: save the new settings
+		return {'type': 'success', 'message': 'Settings saved', 'data': get_v1_settings(form.username)}, 201
 	form_errors = dict()
 	for field_name, error_msgs in form.errors.items():
 		form_errors[field_name] = list()
