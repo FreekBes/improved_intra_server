6145f67d695bb8f7f4bb934e58753dc168b25080
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-10-15 15:54:06 +0200

    add base template for v2 options

--- src/v2/routes/options.py
+++ src/v2/routes/options.py
@@ -1,10 +1,30 @@
+from flask import render_template, session, redirect, url_for
+from src.models.models import Campus
 from ...oauth import authstart
-from flask import session
 from ... import app
 
 
 @app.route('/v2/options')
 def options():
 	if not 'uid' in session:
-		return authstart(2)
-	return 'Coming soon', 200
+		return authstart()
+	return redirect(url_for('options_section', section='improvements'))
+
+
+@app.route('/v2/options/<section>')
+def options_section(section):
+	if not 'uid' in session:
+		return authstart()
+	if section == 'campus':
+		return render_campus_settings()
+	return render_template(f"v2/options/{section}.j2")
+
+
+# Campus-specific settings
+def render_campus_settings():
+	campus:Campus = Campus.query.filter_by(intra_id=int(session['campus'])).first()
+	if not campus:
+		return 'Campus not found', 404
+	if campus.intra_id == 14: # Codam, Amsterdam
+		return render_template('v2/options/codam.j2')
+	return render_template('v2/options/no_campus_settings.j2', campus=campus.name)
