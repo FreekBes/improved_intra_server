ae4086750ea65dabf7ce0212ac0de839a07238ee
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-10-16 02:19:39 +0200

    add client-side options changing

--- src/v2/routes/options.py
+++ src/v2/routes/options.py
@@ -1,7 +1,15 @@
+from src.models.models import BannerImg, BannerPosition, Campus, ColorScheme, Profile, Settings
 from flask import render_template, session, redirect, url_for
-from src.models.models import Campus
 from ...oauth import authstart
 from ... import app
+
+FETCH_DISTRIBUTION = {
+	'improvements': [ 'settings' ],
+	'appearance': [ 'settings', 'color_scheme' ],
+	'profile': [ 'profile' ],
+	'14': [ 'settings' ], # Codam campus, Amsterdam
+	'help': []
+}
 
 
 @app.route('/v2/options')
@@ -12,19 +20,56 @@ def options():
 
 
 @app.route('/v2/options/<section>')
-def options_section(section):
+def options_section(section:str):
 	if not 'uid' in session:
 		return authstart()
+
+	# Set default FETCH_DISTRIBUTION key (defaults to session slug)
+	dist_key:str = section
+
+	# Get campus data if required
+	campus:Campus = None
 	if section == 'campus':
-		return render_campus_settings()
-	return render_template(f"v2/options/{section}.j2")
+		campus:Campus = Campus.query.filter_by(intra_id=int(session['campus'])).first()
+		if not campus:
+			return render_template('v2/options/no_campus_settings.j2', campus='Unknown')
+		dist_key = str(campus.intra_id)
+
+	# Fetch table rows based on the fetch_dist_key
+	settings:Settings = None
+	if 'settings' in FETCH_DISTRIBUTION[dist_key]:
+		settings:Settings = Settings.query.filter_by(user_id=session['uid']).first()
+
+	# Fetch color scheme if required for the page
+	# Dependancy on settings, add it to FETCH_DISTRIBUTION
+	color_scheme:ColorScheme = None
+	if 'color_scheme' in FETCH_DISTRIBUTION[dist_key]:
+		color_scheme:ColorScheme = ColorScheme.query.filter_by(id=settings.colors).first()
+
+	# Fetch profile data if required for the page
+	profile:Profile = None
+	banner_img:BannerImg = None
+	banner_pos:BannerPosition = None
+	if 'profile' in FETCH_DISTRIBUTION[dist_key]:
+		profile:Profile = Profile.query.filter_by(user_id=session['uid']).first()
+		banner_pos = BannerPosition.query.filter_by(id=profile.banner_pos).first()
+		if profile.banner_img:
+			banner_img = BannerImg.query.filter_by(id=profile.banner_img).first()
+
+	# Render page
+	user_settings:tuple = (settings, color_scheme, profile, banner_img, banner_pos)
+	if section == 'campus':
+		return render_campus_settings(dist_key, campus, user_settings)
+	return render_template(f"v2/options/{section}.j2", user_settings=user_settings)
+
+
+@app.route('/v2/options/<section>/save', methods=['POST'])
+def options_section_save(section:str):
+	return 'Not implemented', 501
 
 
 # Campus-specific settings
-def render_campus_settings():
-	campus:Campus = Campus.query.filter_by(intra_id=int(session['campus'])).first()
-	if not campus:
-		return 'Campus not found', 404
+def render_campus_settings(dist_key:str, campus:Campus, user_settings:tuple[Settings, ColorScheme, Profile, BannerImg, BannerPosition]):
 	if campus.intra_id == 14: # Codam, Amsterdam
-		return render_template('v2/options/codam.j2')
+		return render_template('v2/options/codam.j2', user_settings=user_settings)
 	return render_template('v2/options/no_campus_settings.j2', campus=campus.name)
