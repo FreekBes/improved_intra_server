25182a45ad19f3f8ebac1c174d414a3224b47a24
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2023-02-02 16:06:26 +0100

    updated useful staff query

--- useful/queries.sql
+++ useful/queries.sql
@@ -2,4 +2,4 @@
 SELECT COUNT(users.intra_id), campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id GROUP BY campuses.name;
 
 --- See which staff is using the extension
-SELECT users.login, campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id WHERE staff='t';
+SELECT users.login, users.created_at, settings.updated_at, settings.updated_ver, campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id JOIN settings ON settings.user_id = users.intra_id WHERE staff='t' ORDER BY users.created_at;
