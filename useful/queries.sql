8809c408a7fb5796c65b244b60d57004cb498b91
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2023-03-19 18:05:55 +0100

    update useful staff query: implement last_used_at

--- useful/queries.sql
+++ useful/queries.sql
@@ -2,4 +2,4 @@
 SELECT COUNT(users.intra_id), campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id GROUP BY campuses.name;
 
 --- See which staff is using the extension
-SELECT users.login, users.created_at, settings.updated_at, settings.updated_ver, campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id JOIN settings ON settings.user_id = users.intra_id WHERE staff='t' ORDER BY users.created_at;
+SELECT users.login, users.created_at, user_tokens.last_used_at, settings.updated_ver, campuses.name FROM users JOIN campuses ON campuses.intra_id = users.campus_id LEFT JOIN settings ON settings.user_id = users.intra_id LEFT JOIN user_tokens ON user_tokens.user_id = users.intra_id WHERE staff='t' ORDER BY users.created_at;
