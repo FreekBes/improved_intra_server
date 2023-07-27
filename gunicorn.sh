bfb35a41f9ce2a1d55205e29b79ccfa0e6f43369
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2023-07-27 14:22:23 +0200

    temporary fix to prevent concurrent runners
    Runners run per gunicorn worker, which is not great because we end up spamming the Intra API like this. For now, reduce to 1 worker - come up with a better solution later

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,4 +2,4 @@
 cd /opt/improved_intra_server/
 source .venv/bin/activate
 export DEBUG=False
-gunicorn -w 4 --bind 127.0.0.1:8000 --access-logfile logs/access.log --error-logfile logs/error.log wsgi:app
+gunicorn -w 1 --bind 127.0.0.1:8000 --access-logfile logs/access.log --error-logfile logs/error.log wsgi:app
