eb8bf2979d490fe2c6c4aad3f448b343a42c8686
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2023-11-07 01:34:44 +0100

    add support for ics/ical calendars

--- src/v2/routes/events.py
+++ src/v2/routes/events.py
@@ -1,13 +1,15 @@
 import json
 
-from src.lib.auth.decorators import session_required_json
+from src.lib.auth.decorators import ext_token_preferred_json
+from src.lib.auth.tokens import parse_ext_token, create_ext_token
+from flask import session, request, redirect, Response
 from src.models.models import Event, Runner
-from flask import session, Response
-from src import app
+from ics import Calendar, Event as IcsEvent
+from src import app, __version__
 
 
 @app.route('/v2/events', methods=['GET'])
-@session_required_json
+@ext_token_preferred_json
 def events_overview():
 	# Retrieve last fetch date from runner
 	runner:Runner = Runner.query.filter_by(user_id=session['uid']).first()
@@ -42,3 +44,60 @@ def events_overview():
 	)
 	resp.headers['Last-Modified'] = runner.events.strftime('%a, %d %b %Y %H:%M:%S GMT')
 	return (resp.response, resp.status_code, resp.headers.items())
+
+
+@app.route('/v2/events.ics', methods=['GET'])
+@ext_token_preferred_json
+def redir_to_ics():
+	# Generate ext_token for user and redirect to events_ics endpoint
+	ext_token = create_ext_token(session['uid'])
+	return redirect("{}v2/events/{}.ics".format(request.url_root, ext_token.encode().hex()), code=302)
+
+
+# Get iCal calendar for events a user is subscribed to
+# User is authenticated based on an ext_token (this URL should thus never be shared, as it would allow anyone to access the user's session)
+@app.route('/v2/events/<hextoken>.ics', methods=['GET'])
+def events_ics(hextoken:str):
+	# Convert hex token to ext_token
+	try:
+		ext_token = bytes.fromhex(hextoken).decode('utf-8', 'strict')
+	except:
+		return "400 Bad Request", 400
+
+	# Check if ext_token belongs to user
+	try:
+		user, user_token = parse_ext_token(ext_token)
+	except:
+		return "401 Unauthorized", 401
+
+	# Retrieve last fetch date from runner
+	runner:Runner = Runner.query.filter_by(user_id=session['uid']).first()
+	if not runner or not runner.events:
+		return "404 Not Found", 404
+
+	# Retrieve all events for the user
+	events:list[Event] = Event.query.filter_by(user_id=user.intra_id).all()
+
+	# Create iCal calendar
+	cal = Calendar(creator='-//Improved Intra Server v{}//NONSGML v1.0//EN'.format(__version__))
+	for event in events:
+		cal.events.add(IcsEvent(
+			name = bytes.fromhex(event.name).decode('utf-8', 'ignore'),
+			begin = event.begin_at,
+			end = event.end_at,
+			uid = str(event.intra_id),
+			description = bytes.fromhex(event.description).decode('utf-8', 'ignore').replace('\r\n', '\n'),
+			created = event.created_at,
+			last_modified = event.updated_at,
+			location = bytes.fromhex(event.location).decode('utf-8', 'ignore'),
+			url = "https://profile.intra.42.fr/{}/{}".format(("exams" if event.is_exam else "events"), event.intra_id),
+			categories = [event.kind],
+			transparent = False,
+			status = 'CONFIRMED',
+			classification = 'PRIVATE',
+		))
+
+	# Create response with additional headers
+	return cal.serialize(), 200, {'Content-Type': 'text/calendar', 'Last-Modified': runner.events.strftime('%a, %d %b %Y %H:%M:%S GMT'), 'Content-Disposition': 'attachment; filename="events-{}.ics"'.format(user.login)}
+	# Use the line below for debugging plaintext
+	# return cal.serialize(), 200, {'Content-Type': 'text/plain', 'Last-Modified': runner.events.strftime('%a, %d %b %Y %H:%M:%S GMT')}
